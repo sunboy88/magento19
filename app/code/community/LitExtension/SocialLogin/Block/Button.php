4ae9f5151321bc175927b43ba33f9cec5a73ae22
sunboy88 <sunboy2210@gmail.com>
2014-09-08 07:06:58 +0700

    Update FB login

--- app/code/community/LitExtension/SocialLogin/Block/Button.php
+++ app/code/community/LitExtension/SocialLogin/Block/Button.php
@@ -18,10 +18,10 @@ class LitExtension_SocialLogin_Block_Button extends Mage_Core_Block_Template{
 
     protected function _addButtons(){
         $this->_addButton(new LitExtension_SocialLogin_Block_Button_Type_Facebook());
-        $this->_addButton(new LitExtension_SocialLogin_Block_Button_Type_Google());
-        $this->_addButton(new LitExtension_SocialLogin_Block_Button_Type_Linkedin());
-        $this->_addButton(new LitExtension_SocialLogin_Block_Button_Type_Twitter());
-        $this->_addButton(new LitExtension_SocialLogin_Block_Button_Type_Yahoo());
+        // $this->_addButton(new LitExtension_SocialLogin_Block_Button_Type_Google());
+        // $this->_addButton(new LitExtension_SocialLogin_Block_Button_Type_Linkedin());
+        // $this->_addButton(new LitExtension_SocialLogin_Block_Button_Type_Twitter());
+        // $this->_addButton(new LitExtension_SocialLogin_Block_Button_Type_Yahoo());
     }
 
     protected function _addButton(LitExtension_SocialLogin_Block_Button_Type $button){
